87a66fac47918c36bce8233760eedbd93c2196c3
Oussama Lmekkeddem <lmousolm@gmail.com>
2024-07-17 17:38:10 +0100

    i hope fin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,43 @@
 import dynamic from "next/dynamic";
 
+const Loading = () => <p>Loading...</p>;
 
-const DynamicHero = dynamic(() => import("@/app/pages/hero/Hero"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
-const DynamicAbout = dynamic(() => import("@/app/pages/about/About"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
-const DynamicExperience = dynamic(() => import("@/app/pages/experience/Experience"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
-const DynamicProject = dynamic(() => import("@/app/pages/project/Project"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
-const DynamicContact = dynamic(() => import("@/app/pages/contact/Contact"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
-const DynamicSetupUpArrow = dynamic(() => import("@/app/pages/setupUpArrow/SetupUpArrow"),{
-    ssr: false, loading: () => <p> Loading... </p>,});
+const DynamicHero = dynamic(() => import("@/app/pages/hero/Hero"), {
+    ssr: false,
+    loading: Loading,
+});
+const DynamicAbout = dynamic(() => import("@/app/pages/about/About"), {
+    ssr: false,
+    loading: Loading,
+});
+const DynamicExperience = dynamic(() => import("@/app/pages/experience/Experience"), {
+    ssr: false,
+    loading: Loading,
+});
+const DynamicProject = dynamic(() => import("@/app/pages/project/Project"), {
+    ssr: false,
+    loading: Loading,
+});
+const DynamicContact = dynamic(() => import("@/app/pages/contact/Contact"), {
+    ssr: false,
+    loading: Loading,
+});
+const DynamicSetupUpArrow = dynamic(() => import("@/app/pages/setupUpArrow/SetupUpArrow"), {
+    ssr: false,
+    loading: Loading,
+});
 
 export default function Home() {
-  return (
-    <main>
-      <DynamicHero idName="hero"/>
-      <DynamicAbout idName="about"/>
-      <div className={`w-full h-[220px]`}/>
-      <DynamicExperience idName="experience"/>
-      <div className={`w-full h-[240px]`}/>
-      <DynamicProject idName="project"/>
-      <DynamicContact idName="contact"/>
-      <DynamicSetupUpArrow/>
-    </main>
-  );
+    return (
+        <main>
+            <DynamicHero idName="hero" />
+            <DynamicAbout idName="about" />
+            <div className="w-full h-[220px]" />
+            <DynamicExperience idName="experience" />
+            <div className="w-full h-[240px]" />
+            <DynamicProject idName="project" />
+            <DynamicContact idName="contact" />
+            <DynamicSetupUpArrow />
+        </main>
+    );
 }
