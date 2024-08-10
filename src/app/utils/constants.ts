afb365bc6dc2e2e68167fda000c0d65c1d287cb7
Oussama Lmekkeddem <lmousolm@gmail.com>
2024-08-10 17:37:54 +0100

    i hope fin

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -689,7 +689,7 @@ const personnelInfos = {
             'Freelancer'
         ],
         description: 'I am a serious and motivated second-year student at an engineering school, specializing in computer engineering.',
-        resume: 'https://drive.google.com/file/d/105FdXZ9U9ztuAklxWJJgoFGJVnIhi5h7/view?usp=sharing'
+        resume: 'https://drive.google.com/file/d/1rR5DzKLyf3sbg9r_JrzBUofiRBJa09IN/view?usp=drive_link'
 };
 
 const educations = [
